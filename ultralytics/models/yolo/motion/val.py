f06b3b950476583e9b8982985ee1f191fae5b1a1
sfy <actions@gitea.local>
2026-02-02 11:22:57 +0800

    update flow config

--- ultralytics/models/yolo/motion/val.py
+++ ultralytics/models/yolo/motion/val.py
@@ -1,5 +1,21 @@
+import torch
+from typing import Optional
+
 from ..detect.val import DetectionValidator
+from ultralytics.data import build_motion_dataset
 
 
 class MotionValidator(DetectionValidator):
-    pass
+    def build_dataset(self, img_path: str, mode: str = "val", batch: Optional[int] = None) -> torch.utils.data.Dataset:
+        """Build YOLO Dataset.
+
+        Args:
+            img_path (str): Path to the folder containing images.
+            mode (str): `train` mode or `val` mode, users are able to customize different augmentations for each mode.
+            batch (int, optional): Size of batches, this is for `rect`.
+
+        Returns:
+            (Dataset): YOLO dataset.
+        """
+        return build_motion_dataset(self.args, img_path, batch, self.data, mode=mode, stride=self.stride)
+
